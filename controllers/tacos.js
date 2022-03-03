134fcb41c563d2d2d145aa21c58b7467bc608615
princejonaa <jonathanbonner128@gmail.com>
2022-03-03 15:48:18 -0500

    created tacos & controller

---
+++ controllers/tacos.js
@@ -0,0 +1,11 @@
+import {
+  Taco
+} from "../models/taco.js";
+
+function index(req, res) {
+  console.log("TACOS 🌮");
+}
+
+export {
+  index
+};
---
+++ routes/tacos.js
@@ -0,0 +1,11 @@
+import { Router } from 'express'
+
+const router = router()
+
+// GET - localhost:3000/tacos
+
+rouer.get('/', )
+
+export {
+  router
+}
